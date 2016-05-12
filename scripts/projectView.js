3da5e12bc4d554fe9be25c8488a84c817d159cbb
Jeremy Moore <damoores@gmail.com>
2016-05-12 01:11:37 -0700

    add hide/show for body text. add tab navigation. neither work currently

---
+++ scripts/projectView.js
@@ -0,0 +1,25 @@
+var projectView = {};
+
+projectView.handleMainNav = function() {
+  $('.site-navbar').on('click', 'li', function(e) {
+    e.preventDefault();
+    $('.tab-content').hide();
+    var show =$(this).attr('data-content');
+    $('#' + show).show();
+  });
+  $('.site-navbar .tab:first').click();
+};
+
+projectView.setTeasers = function() {
+  $('.project-body *:nth-of-type(n+1)').hide();
+  $('read-on').on('click', function(e) {
+    e.preventDefault();
+    $('project-body *:nth-of-type(n+1)').show();
+    $('read-on').hide();
+  });
+};
+
+$(document).ready( function() {
+  projectView.handleMainNav();
+  projectView.setTeasers();
+});
