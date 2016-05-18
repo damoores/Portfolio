6b05daf8a1e23c0a1049bc6dcd78af8bd2d99af3
Jeremy Moore <damoores@gmail.com>
2016-05-17 22:54:43 -0700

    finish class 07

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -10,11 +10,26 @@ projectView.handleMainNav = function() {
 
 projectView.setTeasers = function() {
   $('.project-body *:nth-of-type(n+2)').hide();
+
   $('#projects').on('click', 'a.read-on', function(e) {
     e.preventDefault();
     $(this).parent().find('*').fadeIn();
     $(this).hide();
   });
+};
+
+projectView.setFunFact = function() {
+  var elements = document.getElementsByTagName('*');
+  var countElements = function() {
+    var splitResult = [].reduce.call(elements, function (a,b) {
+      var results = a += b;
+      return (results);
+    }, [] );
+    $('#fun-fact').append('<p>There are ' + elements.length + ' HTML tags on this page.</p>' );
+    $('#fun-fact').append('<p>That means I typed ' + splitResult.length + ' characters. If I got them all right the first time. Which I did not.</p>' );
+    return console.log(splitResult.length);
+  };
+  countElements();
 };
 
 projectView.initIndexPage = function() {
@@ -23,4 +38,5 @@ projectView.initIndexPage = function() {
   });
   projectView.handleMainNav();
   projectView.setTeasers();
+  projectView.setFunFact();
 };
